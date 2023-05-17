93692fa38dd09e472922687f6e0bf5f056a6282e
Nicholas Zachary <39927058+booksftw@users.noreply.github.com>
2023-05-16 17:25:30 -0700

    Base Login form with Formik and yup complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Login from './components/login/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Login />
   </React.StrictMode>,
 )
