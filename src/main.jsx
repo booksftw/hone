3c4cee9ed9af2c4801ccbca534d23812b6858d4d
Nicholas Zachary <39927058+booksftw@users.noreply.github.com>
2023-05-17 16:52:21 -0700

    Created landLord profile component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Login from './components/login/Login.jsx'
+import LandlordProfile from './components/login/landlordProfile/LandlordProfile.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* Debugging/Testing Login */}
+    {/* Debugging/Testing */}
     {/* <Login /> */}
+    {/* <LandlordProfile /> */}
   </React.StrictMode>,
 )
