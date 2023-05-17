f56701e32fc75274fafae0f9b16eb98f33e2d082
Nicholas Zachary <39927058+booksftw@users.noreply.github.com>
2023-05-16 17:37:09 -0700

    Update App

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import Login from './components/login/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Login />
+    <App />
+    {/* Debugging/Testing Login */}
+    {/* <Login /> */}
   </React.StrictMode>,
 )
